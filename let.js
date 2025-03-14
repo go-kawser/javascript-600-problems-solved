a7a17ee7009da87d161cfd65182315bbb9601fe1
Kawser420 <muhammadkawser202@gmail.com>
2025-03-14 23:48:17 +0600

    some let problem solve and example

---
+++ let.js
@@ -0,0 +1,41 @@
+// javascript let and let it changed and const not changed
+
+// problem------------------------->45
+let age = 24;
+console.log(age);
+age = 25;
+console.log(age);
+
+// problem------------------------->46
+const pocketMoney = 150;
+console.log(pocketMoney);
+
+// problem------------------------->47
+let studyTime = 4;
+studyTime = 0;
+console.log(studyTime);
+
+// problem------------------------->48
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+// problem------------------------->49
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+// problem------------------------->50
+let chairsCount = 40;
+chairsCount = 38;
+console.log(chairsCount);
+chairsCount = 32;
+console.log(chairsCount);
+
+// problem------------------------->51
+let playTime = 3;
+console.log(playTime);
+playTime = 2;
+console.log(playTime);
+playTime = 0;
+console.log(playTime);
