75212a599c1e5b061dd83dbd9f1cb57d38618028
Kawser420 <muhammadkawser202@gmail.com>
2025-03-15 00:41:03 +0600

    some nameCase js added

---
+++ nameCase.js
@@ -0,0 +1,40 @@
+// problem------------------------->59
+
+const myFavoriteFood = "Rice";
+console.log(myFavoriteFood);
+
+// problem------------------------->60
+
+// yes, this is a case-sensitive
+const city = "Chittagong";
+console.log(city);
+const City = "Dhaka";
+console.log(City);
+
+// problem------------------------->61
+const userName = "Johan";
+console.log(userName);
+const username = "Jovan";
+console.log(username);
+
+// problem------------------------->62
+const myHomeAddress = "Dhalapara, Ghatail, Tangail";
+console.log(myHomeAddress);
+
+// problem------------------------->63
+
+//  no output and error because super is a JavaScript KeyWord
+// const super = 12;
+// console.log(super)
+
+// problem------------------------->64
+
+// Unexpected token because class is a JavaScript KeyWord
+// const class = 12;
+// console.log(class)
+
+// problem------------------------->65
+
+// this variable is declare totally wrang. javascript rule and regulation do not declare number at first
+// const 2ndPosition = "virat";
+// console.log(2ndPosition);
