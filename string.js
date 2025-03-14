8925bc8847a52fb813629adac8817bf95e2eaf75
Kawser420 <muhammadkawser202@gmail.com>
2025-03-14 19:38:17 +0600

    some string example added

---
+++ string.js
@@ -0,0 +1,29 @@
+// some string variable
+
+// problem------------------>15
+const name = "Muhammad Kawser";
+console.log(name);
+
+// problem------------------------->16
+const address = "Tangail";
+console.log(address);
+
+// problem------------------------->17
+const friend = "Dhalapara College, Ghatail, Tangail";
+console.log(friend);
+
+// problem------------------------->18
+const fan = "Maria";
+console.log(fan);
+
+// problem------------------------->19
+const movieOfTheYear = "Captain America";
+console.log(movieOfTheYear);
+
+// problem------------------------->20
+const hobby = "cricket, football, coding, programming";
+console.log(hobby);
+
+// problem------------------------->21
+const color = "Black, White, Blue";
+console.log(color);
