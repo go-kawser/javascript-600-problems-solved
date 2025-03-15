d3fe7956b35264168f763ea7b7d305f8af16a6fc
Kawser420 <muhammadkawser202@gmail.com>
2025-03-15 17:59:51 +0600

    some example if-else now

--- if-else.js
+++ if-else.js
@@ -55,3 +55,89 @@ if (temperature <= 35) {
 } else {
   console.log("not condition only combol");
 }
+
+// problem------------------------->101
+const age = 18;
+const height = 60;
+if (age < 20 && height < 70) {
+  console.log("push the car");
+} else {
+  console.log("siting down the bus");
+}
+
+// problem------------------------->102
+const mathMark = 80;
+const englishMark = 85;
+if (mathMark > 80 || englishMark < 87) {
+  console.log("He will admitted");
+} else {
+  console.log("you can not marriage");
+}
+
+// problem------------------------->103
+const gpa = 5;
+const earn = 9000;
+if (5 === 5 && earn < 10000) {
+  console.log("he Got it scholarship");
+} else {
+  console.log("he is not for allow in scholarship");
+}
+
+// problem------------------------->104
+const examCandidateAge = 25;
+const experience = 3;
+
+if (examCandidateAge < 30 && experience > 2) {
+  console.log("job candidate");
+} else {
+  console.log("not job candidate");
+}
+
+// problem------------------------->105
+const egg = 15;
+const isChicken = false;
+
+if (egg > 12 && isChicken === true) {
+  console.log("egg kima i will cook");
+} else {
+  console.log("i will eat Bread and banana");
+}
+
+// problem------------------------->106
+const temperature2 = 100;
+const toCough = true;
+
+if (temperature < 102 || true) {
+  console.log("he will admit doctor");
+} else {
+  console.log("he will bed in the room");
+}
+
+// problem------------------------->107
+const presentSir = 80;
+const autoFail = true;
+
+if (presentSir < 90 || false) {
+  console.log("he is passed");
+} else {
+  console.log("auto fail");
+}
+
+// problem------------------------->108
+const electricity = false;
+const mobileChared = false;
+if (electricity === false && mobileChared === false) {
+  console.log("read the book right now");
+} else {
+  console.log("i will mobile game paly now");
+}
+
+// problem------------------------->109
+
+const shartPrice = 1200;
+const discount = 20;
+if (shartPrice > 1000 && 20 === 20) {
+  console.log("i will buy shart now");
+} else {
+  console.log("i do not buy shart");
+}
