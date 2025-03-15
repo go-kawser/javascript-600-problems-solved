2a991ba8a52e4e9671e2ad34b741b6ee975210b4
Kawser420 <muhammadkawser202@gmail.com>
2025-03-15 16:36:30 +0600

    if else conditional comparison tag is ready to problem solved now

---
+++ if-else.js
@@ -0,0 +1,57 @@
+// problem------------------------->95
+const homeBackTime = 6;
+if (6 >= 6) {
+  console.log("i was coming home on time");
+} else {
+  console.log("i am not on time");
+}
+
+// problem------------------------->96
+const login = true;
+if (login == true) {
+  console.log("Welcome!");
+} else {
+  console.log("Get Lost!");
+}
+if (login == false) {
+  console.log("Welcome!");
+} else {
+  console.log("Get Lost!");
+}
+
+// problem------------------------->97
+const iAmRunning = 5;
+if (iAmRunning > 0) {
+  console.log(
+    "i am running every day 5 kilometer because he's give me chocolate"
+  );
+} else {
+  console.log("if i'm not running every day that not healthy good");
+}
+
+// problem------------------------->98
+const wantMoreThan = 80;
+const moreThanIGotIt = 85;
+if (wantMoreThan < moreThanIGotIt) {
+  console.log("i win the bike");
+} else {
+  console.log("i loss the bike");
+}
+
+// problem------------------------->99
+const movieShowWatch = 9;
+const movieDoNotWatch = 10;
+
+if (movieShowWatch > movieDoNotWatch) {
+  console.log("i will watch the movie show");
+} else {
+  console.log("i do not show watch movie because i am sleeping early");
+}
+
+// problem------------------------->100
+const temperature = 30;
+if (temperature <= 35) {
+  console.log("i play air conditioner");
+} else {
+  console.log("not condition only combol");
+}
