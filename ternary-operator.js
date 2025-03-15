c07008e6352a822e421a6dc4192e4d27f15bc866
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 01:00:37 +0600

    ternary operator some problem solved

---
+++ ternary-operator.js
@@ -0,0 +1,52 @@
+// problem------------------------->122
+let cashback = 0;
+if (cashback >= 3000) {
+  cashback = 500;
+  console.log(cashback, "cashback 500 taka");
+} else {
+  cashback = 0;
+  console.log(cashback, "no cashback");
+}
+
+//ternary operator
+let = 0;
+cashback =
+  cashback >= 3000
+    ? (console.log(500, "cashback 500 taka"), 500)
+    : (console.log(0, "no cashback"), 0);
+
+// problem------------------------->123
+// let ageDefine = 20;
+// if (ageDefine >= 15) {
+//   console.log("Teenager");
+// } else {
+//   console.log("Child");
+// }
+
+//ternary operator
+let ageDefine = 20;
+let category = ageDefine >= 15 ? "Teenager" : "Child";
+console.log(category);
+
+// problem------------------------->124
+const isLoggedIn = true;
+const result = isLoggedIn === false ? "Welcome back" : "Please Login";
+console.log(result);
+
+// problem------------------------->125
+const oilTank = true;
+const full = oilTank === true ? "Ready for a long drive" : "Fill the tank";
+console.log(full);
+
+// problem------------------------->126
+let exam = false;
+let pass = exam === true ? "Party Time " : "Next semester e serious study do";
+console.log(pass);
+
+// problem------------------------->127
+const sunny = false;
+console.log("Stay Home");
+
+// problem------------------------->128
+const Expensive = false;
+console.log("will buy ths item");
