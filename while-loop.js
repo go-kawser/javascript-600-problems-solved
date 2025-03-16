6d0d386b2b01a7521fa1e9bddda3b3fd11b2d0b7
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 12:14:48 +0600

    while loop add, odd, multiplication, divition all proble solved

--- while-loop.js
+++ while-loop.js
@@ -34,3 +34,48 @@ while (num2 >= 110) {
   console.log(divide);
   num2--;
 }
+
+// problem------------------------->164
+let num3 = 50;
+while (num3 < 100) {
+  console.log(num3);
+  num3++;
+}
+
+// problem------------------------->165
+let num4 = 5;
+let sum = 0;
+while (num4 <= 15) {
+  sum = sum + num4;
+  console.log("Sum:", sum);
+  num4++;
+}
+
+// problem------------------------->166
+let rollIncrease = 1;
+let rollSum = 0;
+while (rollIncrease <= 50) {
+  rollSum = rollSum + rollIncrease;
+  console.log("Total Roll Sum:", rollSum);
+  rollIncrease++;
+}
+
+// problem------------------------->167
+let num5 = 21;
+let sum2 = 0;
+while (num5 < 50) {
+  sum2 = sum2 + num5;
+  console.log(sum2);
+  num5++;
+}
+
+// problem------------------------->168
+let digitNumber = 20;
+let digitAdd = 0;
+let lastNumber = 40;
+
+while (digitNumber <= lastNumber) {
+  digitAdd = digitAdd + lastNumber;
+  digitNumber++;
+}
+console.log("Sum Last Number:", digitAdd);
