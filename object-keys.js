ccf5eae074e201c224bac5ffda1ce99749051e38
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 18:51:01 +0600

    oject keys problem solved

---
+++ object-keys.js
@@ -0,0 +1,32 @@
+// problem------------------------->199
+const books = {
+  name: "jhankar",
+  written: "mahbub",
+  price: 570,
+  age: 35,
+  chid: 2,
+  quality: "awesome",
+};
+const keys = Object.keys(books);
+const values = Object.values(books);
+console.log(keys);
+console.log(values);
+
+// problem------------------------->200
+const bike = {
+  brand: "Hero",
+  price: 120000,
+  model: "Splendor",
+};
+
+const value = Object.values(bike);
+console.log(value);
+
+// problem------------------------->201
+const player = {
+  name: "Messi",
+  team: "Argentina",
+  goals: 91,
+};
+const valuesOfMessi = Object.values(player);
+console.log(valuesOfMessi);
