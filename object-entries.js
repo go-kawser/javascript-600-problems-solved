5e0620260d51861bee6fe112a99d187c2db6a8c8
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 19:18:11 +0600

    object entries problem solved right now is done

---
+++ object-entries.js
@@ -0,0 +1,8 @@
+// extra problem solved in object entries
+const person = {
+  name: "Alice",
+  age: 25,
+  country: "Bangladesh",
+};
+const entries = Object.entries(person);
+console.log(entries);
