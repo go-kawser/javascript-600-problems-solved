70e38a05b3cb3ad5f7de6bc2a2bc0dc33aad4edb
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 11:27:02 +0600

    while loop problem solved

---
+++ while-loop.js
@@ -0,0 +1,36 @@
+// problem------------------------->159
+let num = 1;
+while (num <= 10) {
+  console.log("i will practice code every day in 3 hours In Sha Allah");
+  num++;
+}
+
+// problem------------------------->160
+let number = 1;
+while (number < 20) {
+  console.log(number);
+  number++;
+}
+
+// problem------------------------->161
+let digit = 50;
+while (digit < 100) {
+  console.log(digit);
+  digit++;
+}
+
+// problem------------------------->162
+let numbers = 1;
+while (numbers < 10) {
+  const multiple = numbers * 3;
+  console.log(multiple);
+  numbers++;
+}
+
+// problem------------------------->163
+let num2 = 111;
+while (num2 >= 110) {
+  divide = num2 / 2;
+  console.log(divide);
+  num2--;
+}
