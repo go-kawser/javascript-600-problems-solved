a3790e0f95a485d873c380f77470b7981d0a391c
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 19:39:13 +0600

    object for in loop some problem solved

--- object-property.js
+++ object-property.js
@@ -1,4 +1,14 @@
-// extra problem solved in object property
+// problem------------------------->204
+const article = {
+  title: "LearningJs",
+  category: "Programming",
+};
+
+const keys = Object.keys(article);
+const hasAuthor = keys.includes("author");
+console.log(hasAuthor);
+
+// problem------------------------->205
 const profile = {
   name: "Rahim",
   age: 28,
