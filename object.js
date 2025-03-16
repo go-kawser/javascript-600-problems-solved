3c3db6ff5858cd54576e49cdb01523e047ca8710
Kawser420 <muhammadkawser202@gmail.com>
2025-03-16 15:20:38 +0600

    some object addedd

---
+++ object.js
@@ -0,0 +1,53 @@
+// problem------------------------->184
+const teacher = {
+  name: "jhankar",
+  sub: "code",
+  subName: "javascript",
+  age: 30,
+  home: "America",
+};
+
+// problem------------------------->185
+const tree = {
+  isTree: true,
+  treeName: "mango",
+  height: 30,
+  age: 20,
+  fruit: "mango",
+};
+
+// problem------------------------->186
+const animals = {
+  name: ["cat", "dog", "itor", "tlapuka", "person", "man", "woman"],
+};
+
+// problem------------------------->187
+const father = {
+  name: "Taijuddin",
+  profession: "businessman",
+  age: 47,
+  child: 4,
+};
+
+// problem------------------------->188
+const motorBike = {
+  brand: "yamaha",
+  wheels: 2,
+  color: "red",
+  speed: 300,
+};
+
+// problem------------------------->189
+const bird = {
+  name: "quail",
+  colo: "black",
+  food: "rice",
+};
+
+// problem------------------------->190
+const laptop = {
+  brand: "hp",
+  processor: 3,
+  price: 120000,
+  display: 32,
+};
