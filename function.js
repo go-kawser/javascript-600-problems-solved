041b0a50ed1b9e06a3e1f659c084d59244d954d0
Kawser420 <muhammadkawser202@gmail.com>
2025-03-20 00:36:58 +0600

    function problem solveing

---
+++ function.js
@@ -0,0 +1,44 @@
+// problem solved--------------------------->215
+function juice() {
+  console.log("ai new lebor juice");
+}
+juice();
+
+// problem solved--------------------------->216
+function myRoutine() {
+  console.log("just sleeping the morning");
+}
+myRoutine();
+
+// problem solved--------------------------->217
+function myFamily() {
+  console.log(4);
+  console.log(
+    "muhammad",
+    "kawser",
+    "kawser",
+    "maria",
+    "maria",
+    "maria",
+    "maria"
+  );
+}
+myFamily();
+
+// problem solved--------------------------->218
+function taskDone() {
+  console.log("codings");
+  console.log("mobile");
+  console.log("song");
+  console.log("programming");
+  console.log("active");
+}
+taskDone();
+
+// problem solved--------------------------->219
+function greetMet() {
+  console.log("Good Morning! Have a great day");
+}
+greetMet();
+
+// problem solved--------------------------->220
