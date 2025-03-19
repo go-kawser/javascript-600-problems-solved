5814db315fa7c45a445e990dbf2bab92090049e3
Kawser420 <muhammadkawser202@gmail.com>
2025-03-20 00:56:07 +0600

    father and son total age

---
+++ function-parameter.js
@@ -0,0 +1,8 @@
+// problem solved--------------------------->222
+function difference(num1, num2) {
+  const total = num1 + num2;
+  console.log(total);
+}
+const fatherAge = 48;
+const sonAge = 24;
+difference(fatherAge, sonAge);
