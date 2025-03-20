4a5534c4daaf6d74993cfb0a9b8f850685ce1744
Kawser420 <muhammadkawser202@gmail.com>
2025-03-21 01:58:44 +0600

    function parameter is done

--- function-parameter.js
+++ function-parameter.js
@@ -6,3 +6,12 @@ function difference(num1, num2) {
 const fatherAge = 48;
 const sonAge = 24;
 difference(fatherAge, sonAge);
+
+// problem solved--------------------------->223
+function makeCalculator(n, m) {
+  const multiple = n * m;
+  console.log(multiple);
+}
+const number = 20;
+const number2 = 15;
+makeCalculator(number, number2);
