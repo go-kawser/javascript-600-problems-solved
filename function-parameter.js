a1d38c74e49e7de7462313af2e12d2d2822820df
Kawser420 <muhammadkawser202@gmail.com>
2025-03-21 23:02:43 +0600

    some function example problem  solved

--- function-parameter.js
+++ function-parameter.js
@@ -15,3 +15,19 @@ function makeCalculator(n, m) {
 const number = 20;
 const number2 = 15;
 makeCalculator(number, number2);
+
+// problem solved--------------------------->224
+function threeSubject(sub, sub2, sub3) {
+  const total = sub + sub2 + sub3;
+  console.log(total);
+}
+threeSubject(67, 90, 86);
+
+// problem solved--------------------------->225
+function threeSubject(sub, sub2, sub3) {
+  return sub + sub2 + sub3;
+}
+const subNumber = threeSubject(89, 56, 88);
+console.log(subNumber);
+
+// problem solved--------------------------->225
