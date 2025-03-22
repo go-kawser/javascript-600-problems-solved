5e92174328ad691d295a170d9949dfa78184fccf
Kawser420 <muhammadkawser202@gmail.com>
2025-03-23 01:19:27 +0600

    function basic example problem solved

--- function-basic-array.js
+++ function-basic-array.js
@@ -1,17 +1,1 @@
 // problem solved--------------------------->238
-function isEvenOrOdd(num) {
-  for (let num of numbers) {
-    if (num % 2 === 0) {
-      return num;
-    }
-  }
-}
-let numbers = [12, 44, 55, 82, 33, 0, 10, 9];
-console.log(isEvenOrOdd(numbers));
-
-// problem solved--------------------------->239
-function firstLatter(str) {
-  const letter = str.length;
-  console.log(letter[1]);
-}
-firstLatter("kamal");
--- function-basic-practice.js
+++ function-basic-practice.js
@@ -1,13 +1,49 @@
 // function step by step practice and exercise
 
 // step-01
+// --------------------------------------->
 // function evenSizedString(str) {
 //   console.log(str);
 // }
 // evenSizedString("Dhaka");
 
 // step-02
+// ----------------------------------------->
+// function evenSizedString(str) {
+//   let len = str.length;
+//   console.log(len, str);
+// }
+// evenSizedString("Dhaka");
+
+//step-03
+// ------------------------------------------------>
+// function evenSizedString(str) {
+//   let len = str.length;
+//   if (len % 2 === 0) {
+//     console.log("even size");
+//   } else {
+//     console.log("odd size");
+//   }
+// }
+// evenSizedString("Dhaka");
+
+// step-04
+// ---------------------------------------------->
 function evenSizedString(str) {
-  console.log(str);
+  let len = str.length;
+  if (len % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
-evenSizedString("Dhaka");
+const isEven = evenSizedString("Dhaka");
+console.log(isEven);
+
+// another example
+function numberOfElement(number) {
+  const len = number.length;
+  return len;
+}
+const len = [12, 354, 66, 77, 23, 3, 6, 7];
+console.log(len);
