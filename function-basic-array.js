bf0894296fd492da4f5792a6c57debc5c685e93d
Kawser420 <muhammadkawser202@gmail.com>
2025-03-23 01:40:07 +0600

    array fuciton odd number and even number check

--- function-basic-array.js
+++ function-basic-array.js
@@ -1,1 +1,11 @@
 // problem solved--------------------------->238
+function numberEvenOrOdd(nums) {
+  let num = nums.length;
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const number = [12, 33, 444, 666, 22];
+console.log(numberEvenOrOdd(number));
