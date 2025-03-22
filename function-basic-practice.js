50bb98e69f55c211e60ba9e6e1150b866ec1d1ef
Kawser420 <muhammadkawser202@gmail.com>
2025-03-23 01:08:51 +0600

    funciton step by step practice

---
+++ function-basic-practice.js
@@ -0,0 +1,13 @@
+// function step by step practice and exercise
+
+// step-01
+// function evenSizedString(str) {
+//   console.log(str);
+// }
+// evenSizedString("Dhaka");
+
+// step-02
+function evenSizedString(str) {
+  console.log(str);
+}
+evenSizedString("Dhaka");
