9c9fa05faf0a4a7e47273aad18ac85eb8d4cf56c
Kawser420 <muhammadkawser202@gmail.com>
2025-03-23 16:11:09 +0600

    function-basic practice problem solved

--- function-basic-practice.js
+++ function-basic-practice.js
@@ -47,3 +47,59 @@ function numberOfElement(number) {
 }
 const len = [12, 354, 66, 77, 23, 3, 6, 7];
 console.log(len);
+
+// another example
+// step no---------------------------->01 // basic function declare korbo
+// function sumOfNumbers() {}
+// const result = sumOfNumbers();
+// console.log(result);
+
+// step no---------------------------->02 // ekta array parameter set korb and parameter pass korbo
+// function sumOfNumbers(numbers) {
+//   console.log(numbers);
+// }
+// const result = sumOfNumbers([12, 34, 55, 89]);
+// console.log(result);
+
+// step no---------------------------->03 // tarpor array er moddo loop chalabo , dekhbo protita upadan alada alada vabe pawa jai kina
+// function sumOfNumbers(numbers) {
+//   for (number of numbers) {
+//     console.log(number);
+//   }
+// }
+// const result = sumOfNumbers([12, 44, 56, 986]);
+// console.log(result);
+
+// step no---------------------------------->04 // sum kore shei sumke ekta jaugai store krobo
+// function sumOfNumbers(numbers) {
+//   let sum = 0;
+//   for (number of numbers) {
+//     sum = sum + number;
+//     console.log(sum);
+//   }
+// }
+// const result = sumOfNumbers([123, 33, 55, 8, 99]);
+// console.log(result);
+
+// step no--------------05 //
+
+function sumOfNumbers(numbers) {
+  let sum = 0;
+  for (const number of numbers) {
+    sum = sum + number;
+  }
+  return sum;
+}
+const result = sumOfNumbers([12, 12, 10, 55, 98, 22]);
+console.log(result);
+
+// new another example even number plus, i do it...........
+function sumOfEvenNumber(numbers) {
+  let sumEven = 0;
+  for (const number of numbers) {
+    if (number % 2 === 0) sumEven = sumEven + number;
+  }
+  return sumEven;
+}
+const evenResult = sumOfEvenNumber([12, 10, 3, 77, 32, 99]);
+console.log(evenResult);
