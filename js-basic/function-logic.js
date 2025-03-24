baa6d55a350141b3209815fae515883add50bed2
Kawser420 <muhammadkawser202@gmail.com>
2025-03-25 00:48:28 +0600

    i just solved this problme now

--- js-basic/function-logic.js
+++ js-basic/function-logic.js
@@ -5,3 +5,11 @@ function mileToGauge(mile) {
   return mile * 1760;
 }
 console.log(mileToGauge(13));
+
+// problem solved------------------------------>253
+// 1 kilowatt hour to kilocalories solve this problem. ok , i just do it now
+function kilowattHoursToKilocalories(kc) {
+  const kilocalorie = kc * 860;
+  return kilocalorie;
+}
+console.log(kilowattHoursToKilocalories(10));
