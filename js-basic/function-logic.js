bb2e5553229a3a247a3135e9e5ad29d7b09327d6
Kawser420 <muhammadkawser202@gmail.com>
2025-03-25 00:39:10 +0600

    logic just build up now and that is the positive thing

--- js-basic/function-logic.js
+++ js-basic/function-logic.js
@@ -1,1 +1,7 @@
 // problem solved------------------------------>252
+// mile to gauge -----------> we know 1 mile = 1760 gauge
+// so, problem solved now
+function mileToGauge(mile) {
+  return mile * 1760;
+}
+console.log(mileToGauge(13));
