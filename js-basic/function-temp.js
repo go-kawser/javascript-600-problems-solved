607681e48afea015fd0f2e38a5ac2c7199b59fb4
Kawser420 <muhammadkawser202@gmail.com>
2025-03-26 02:43:47 +0600

    function simple temp problem sloved

---
+++ js-basic/function-temp.js
@@ -0,0 +1,17 @@
+// extra example // just fun
+// simple exchange
+let a = 10;
+let b = 20;
+console.log(a, b);
+a = b;
+b = a;
+console.log(a, b);
+
+// temp exchange
+let x = 11;
+let y = 65;
+console.log(x, y);
+const temp = x;
+x = y;
+y = temp;
+console.log(x, y);
