325006bbf1cdd49480deb93a23712ff79ec699a2
Kawser420 <muhammadkawser202@gmail.com>
2025-04-08 01:46:38 +0600

    function default porblme solved

---
+++ js-advanced/function-default-practice.js
@@ -0,0 +1,8 @@
+// extra practice
+function laptopBuy(one, two) {
+  const result = one + two;
+  console.log(one, two, result);
+  return result;
+}
+const price = laptopBuy(20000, 30000);
+console.log(price);
--- js-advanced/function-default.js
+++ js-advanced/function-default.js
@@ -1,6 +1,18 @@
 // problem solved------------------------------>268
+function add(num1 = 0, num2 = 0, num3 = 0) {
+  const result = num1 + num2 + num3;
+  console.log(result, num1, num2, num3);
+  return result;
+}
+const total = add(1, 5, 4);
+console.log(total);
 
 // problem solved------------------------------>269
+function deposit(have = 0, money = 50) {
+  const seeMoney = have + money;
+  return seeMoney;
+}
+console.log(deposit());
 
 // problem solved------------------------------>270
 
