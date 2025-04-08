43e62579f2f4d01f4c77bdda22781473a0582a46
Kawser420 <muhammadkawser202@gmail.com>
2025-04-09 00:12:52 +0600

    porblem solved now

--- js-advanced/function-default.js
+++ js-advanced/function-default.js
@@ -15,14 +15,51 @@ function deposit(have = 0, money = 50) {
 console.log(deposit());
 
 // problem solved------------------------------>270
+function nameAndMonthIncome(name = "anonymous", income = 0) {
+  const result = name + income;
+  console.log(name, income, result);
+  return result;
+}
+const monthIncome = nameAndMonthIncome();
+console.log(monthIncome);
 
 // problem solved------------------------------>271
+function squire(number = 1) {
+  const result = number * number;
+  return result;
+}
+console.log(squire(3));
 
 // problem solved------------------------------>272
+// function product(phone = "Unknown product", price = 1) {}
+function product(phone = "Unknown product", price = 1) {
+  const total = `${phone} costs ${price}`;
+  return total;
+}
+console.log(product());
 
 // problem solved------------------------------>273
+function favBooks(fav1 = ["JS book"]) {
+  const result = fav1;
+  return result;
+}
+console.log(favBooks());
 
-// problem solved------------------------------>274
+// problem solved-------------------------------->274
+let product = { price: 3000, quantity: 1 };
+function marketPrice(product) {
+  const total = product.price * product.quantity;
+  return total;
+}
+console.log(marketPrice(100));
+let product = [{ price: 3000, quantity: 1 }];
+
+function marketPrice(product) {
+  const result = product[0].price * product[0].quantity;
+  return result;
+}
+
+console.log(marketPrice(product)); // ✅ Output: 3000
 
 // problem solved------------------------------>275
 
